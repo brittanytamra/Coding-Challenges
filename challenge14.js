0590645ed7758cf041fcb64bb779b0a62c7e140b
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-01 22:44:07 -0500

    Create challenge14.js

---
+++ challenge14.js
@@ -0,0 +1,4 @@
+//Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let string = 'You cannot end a sentence with because because because is a conjunction';
+console.log(string.substr(7,8))

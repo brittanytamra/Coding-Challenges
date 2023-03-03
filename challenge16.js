e54bb275fd84710dc683b486871472fddbbc1d18
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-02 23:48:22 -0500

    Create challenge16.js

---
+++ challenge16.js
@@ -0,0 +1,4 @@
+//Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let strig = 'You cannot end a sentence with because because because is a conjunction'
+console.log(string.match(pattern)) ["because", "because", "because"]

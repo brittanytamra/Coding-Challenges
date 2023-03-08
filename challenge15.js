7416b780c99e0c23ab92a4ce4a94246bd1bdf576
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-07 23:55:02 -0500

    Create challenge15.js

---
+++ challenge15.js
@@ -0,0 +1,10 @@
+//Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+
+let annualIncome;
+
+ function annualIncome(){
+  let total;
+  let income = 5000 + 1000 + 15000
+  let totalIncome = 21000
+}

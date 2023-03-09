5a8acee77c0351675edbe036f0a0d3d5ee7195ce
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-09 00:44:57 -0500

    Create challenge17.js

---
+++ challenge17.js
@@ -0,0 +1,16 @@
+
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+let firstName  = 'John';
+let lastName = 'Smith';
+let country = 'Canada';
+let city = 'Toronto';
+let age = 25;
+let isMarried = 'Yes';
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);

23b36a5be34f08dd00538d43455124e1ae8a155c
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-12 23:57:19 -0400

    Create challenge20.js

---
+++ challenge20.js
@@ -0,0 +1,7 @@
+// 4. Boolean value is either true or false.
+
+ // - Write three JavaScript statement which provide truthy value.
+
+let isWinter = true;
+let isDecember = true;
+let istwentyTwentyThree = true;

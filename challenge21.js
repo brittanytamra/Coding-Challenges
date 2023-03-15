504ccc372b60211c80f5b9761b689e90bd29e69d
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-14 22:21:39 -0400

    Create challenge21.js

---
+++ challenge21.js
@@ -0,0 +1,11 @@
+// Find the length of python and jargon and make a falsy comparison statement.
+
+
+const python = "python";
+const jargon = "jargon";
+
+if (python.length !== jargon.length) {
+  console.log("The length of python and jargon is different.");
+} else {
+  console.log("The length of python and jargon is the same.");
+}

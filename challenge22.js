1352c8a8786b9fac0f800ca15a7384e9f7fa7b02
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-16 22:56:47 -0400

    Create challenge22.js

---
+++ challenge22.js
@@ -0,0 +1,9 @@
+// 1. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+// Enter base: 20
+// Enter height: 10
+// The area of the triangle is 100
+
+function areaOfTriangle(base,height) {
+  let areaOfTriangle = prompt('20', '10')
+}

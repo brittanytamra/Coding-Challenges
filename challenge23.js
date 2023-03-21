7ded05b2cc3cb876f6f5010e0e62693912cb9dff
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-20 22:43:15 -0400

    Create challenge23.js

---
+++ challenge23.js
@@ -0,0 +1,14 @@
+// 2. Write a script that prompt the user to enter side a, side b, and side c of the triangle and calculate the perimeter of triangle (perimeter = a + b + c)
+
+// Enter side a: 5
+// Enter side b: 4
+// Enter side c: 3
+// The perimeter of the triangle is 12
+
+
+function sidesOfTrianle(a,b,c) {
+let sidesOfTrianle = prompt('5','4', '3')
+}
+
+// console.log(a + b + c);
+console.log(areaOfTriangle);

1d2dabeab91abe78dbfd6c7bf14b4b914f913de8
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-22 23:43:27 -0400

    Create challenge24.js

---
+++ challenge24.js
@@ -0,0 +1,8 @@
+//  Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+
+function areaOfTriangle(length,width) {
+   let areaOfTriangle = prompt(2 * (length + width));
+}
+
+console.log(areaOfTriangle);
+

011f6184f5d102f44f4e68e83854f67ab143eb89
Brittany Brathwaite <brittanytb88@gmail.com>
2023-03-26 23:56:32 -0400

    Create challenge25.js

---
+++ challenge25.js
@@ -0,0 +1,6 @@
+// Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+function radiusOfTriangle(pi,r,r){
+  let radiusOfTriangle = prompt(2 * pi * r);
+}
+console.log(radiusOfTriangle);

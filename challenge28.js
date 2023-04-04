e96892b70c15f546a02922c6db17ca0cd623c500
Brittany Brathwaite <brittanytb88@gmail.com>
2023-04-04 01:49:53 -0400

    Create challenge28.js

---
+++ challenge28.js
@@ -0,0 +1,26 @@
+//3.If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+// using if else
+// ternary operator.
+
+
+  // let a = 4
+  // let b = 3
+
+  // 4 is greater than 3
+ 
+
+
+
+// if else
+  if(a > b){
+    console.log('a is greater than b');
+  } else {
+    console.log('a is less than b')
+  }
+
+
+// ternary operator
+
+const aIsGreaterThanB = true;
+  console.log(aIsGreaterThanB? a : b);

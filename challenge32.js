6a2ecb4197639e8b7a7f064e1dc3416c9a5350b3
Brittany Brathwaite <brittanytb88@gmail.com>
2023-04-19 20:27:18 -0400

    Create challenge32.js

---
+++ challenge32.js
@@ -0,0 +1,20 @@
+// 2. Write a program which tells the number of days in a month, now consider leap year.
+
+
+let twelveMonths = prompt('How many days in a month?');
+let daysInaMonth = daysInaMonth.toUpperCase()
+
+switch(daysInaMonth){
+  case 'January':
+    console.log('January has 31 days')
+    break;
+    case 'JANUARY':
+    console.log('January has 31 days')
+    break;
+    case 'February':
+    console.log('February has 28 days')
+    break;
+    case 'FEbruary':
+    console.log('February has 28 days')
+    break;
+}

2dff0f7919e52dbd03d1a19412afafb961da22d2
Brittany Brathwaite <brittanytb88@gmail.com>
2023-05-11 02:10:55 -0400

    Create challenge36.js

---
+++ challenge36.js
@@ -0,0 +1,6 @@
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+const itCompaniesTwo = ['FaceBook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+
+const sentence = companies.join(', ') + ' are big IT companies. '
+console.log(companies);

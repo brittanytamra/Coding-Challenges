16d6c9b930e2caa0f8f676c6ed2d5f7fa420ee74
Brittany Brathwaite <brittanytb88@gmail.com>
2023-07-26 02:04:56 -0400

    Create challenge41.js

---
+++ challenge41.js
@@ -0,0 +1,6 @@
+// Slice out the last 3 companies from the array
+
+const itCompanies = ['FaceBook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+
+
+console.log(itCompanies.slice(-3));

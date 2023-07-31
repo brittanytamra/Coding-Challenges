24a7ed9a42b8282726cf0be705bb15000c39c3c8
Brittany Brathwaite <brittanytb88@gmail.com>
2023-07-31 01:30:23 -0400

    Create challenge42.js

---
+++ challenge42.js
@@ -0,0 +1,7 @@
+// Slice out the middle IT company or companies from the array
+
+
+const itCompanies = ['FaceBook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+
+
+console.log(itCompanies.slice(4));
